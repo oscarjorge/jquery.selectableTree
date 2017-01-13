83df6528d777ac4bb35274ee919ce20063e5c72b
oscarjorge <oscarjorgecastillo@gmail.com>
2017-01-13 11:49:26 +0100

    Update jquery.selectableTree.js

--- js/jquery.selectableTree.js
+++ js/jquery.selectableTree.js
@@ -4,49 +4,24 @@
     // here we go!
     $.selectableTree = function (element, options) {
         if (options != null && options.dataSource != null) {
-            // plugin's default options
-            // this is private property and is  accessible only from inside the plugin
             var defaults = {
-
                 colorInHeader: true,
                 collapsed: true,
-                // if your plugin is event-driven, you may provide callback capabilities
-                // for its events. execute these functions before or after events of your 
-                // plugin, so that users may customize those particular events without 
-                // changing the plugin's code
                 onFoo: function () { }
 
             }
-
-            // to avoid confusions, use "plugin" to reference the 
-            // current instance of the object
             var plugin = this;
-
-            // this will hold the merged default, and user-provided options
-            // plugin's properties will be available through this object like:
-            // plugin.settings.propertyName from inside the plugin or
-            // element.data('pluginName').settings.propertyName from outside the plugin, 
-            // where "element" is the element the plugin is attached to;
             plugin.settings = {}
-
-            var $tree = $(element), // reference to the jQuery version of DOM element
-                 tree = element;    // reference to the actual DOM element
-
-            // the "constructor" method that gets called when the object is created
+            var $tree = $(element), 
+                 tree = element; 
             plugin.init = function () {
-
                 plugin.originalDataSource = options.dataSource;
                 delete options.dataSource;
-                // the plugin's final properties are the merged default and 
-                // user-provided options (if any)
                 plugin.settings = $.extend({}, defaults, options);
-
                 $tree.html('');
                 $tree.addClass('tree').addClass('list-group');
-
                 $(plugin.originalDataSource).each(function (index, parent) {
                     $tree.append("<a class='parent list-group-item' data-id='" + String(parent.Item.Value) + "'><i class='fa fa-angle-up collapsed'/> " + parent.Item.Text + " <span class='badge'></span><i class='fa pull-right check' aria-hidden='true'/></a>");
-
                     var $parentDOM = $tree.children().last();
                     $parentDOM.find('.check').last().on('click', function (e) {
                         selectAll($(this), $tree);
@@ -102,18 +77,9 @@
                     $tree.find('.collapsed').each(function (i, v) {
                         collapse($(this));
                     });
-                    
-
-
             }
 
             // public methods
-            // these methods can be called like:
-            // plugin.methodName(arg1, arg2, ... argn) from inside the plugin or
-            // element.data('pluginName').publicMethod(arg1, arg2, ... argn) from outside 
-            // the plugin, where "element" is the element the plugin is attached to;
-
-            // a public method. for demonstration purposes only - remove it!
             plugin.reset = function () {
                 $tree.data('selectableTree', null);
             }
@@ -143,13 +109,9 @@
             plugin.isDirty = function () {
                 return $tree.data('dirty');
             }
-
             // end public methods
 
             // private methods
-            // these methods can be called only from inside the plugin like:
-            // methodName(arg1, arg2, ... argn)
-
             var getObjectItem = function (item) {
                 if (item.parent().hasClass('parent')) {
                     var objeto = new Object();
@@ -346,39 +308,17 @@
             }
 
             // end private methods
-
-            // fire up the plugin!
-            // call the "constructor" method
             plugin.init();
         }
         else
             console.error('El plugin necesita el parámetro dataSource.');
     }
-
-    // add the plugin to the jQuery.fn object
     $.fn.selectableTree = function (options) {
-
-        // iterate through the DOM elements we are attaching the plugin to
         return this.each(function () {
-
-            // if plugin has not already been attached to the element
             if (undefined == $(this).data('selectableTree')) {
-
-                // create a new instance of the plugin
-                // pass the DOM element and the user-provided options as arguments
                 var plugin = new $.selectableTree(this, options);
-
-                // in the jQuery version of the element
-                // store a reference to the plugin object
-                // you can later access the plugin and its methods and properties like
-                // element.data('selectableTree').publicMethod(arg1, arg2, ... argn) or
-                // element.data('selectableTree').settings.propertyName
                 $(this).data('selectableTree', plugin);
-
             }
-
         });
-
     }
-
 })(jQuery);
